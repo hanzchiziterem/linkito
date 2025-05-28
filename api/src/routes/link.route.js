01974d7930277fe9999f3c5beaa41f34b5b5c9ea
Hanz Chiziterem <hancarta23@gmail.com>
2025-05-28 11:10:35 +0100

    Implement the url routes.

--- api/src/routes/link.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createLink } from "../controllers/link.controller.js";
-const router = express.Router();
-
-router.post("/link", createLink);
-
-export default router;
