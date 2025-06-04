91ab00825440fd4bf5cccacc7d946b85b6ac7440
Hanz Chiziterem <hancarta23@gmail.com>
2025-06-04 11:41:35 +0100

    App File.

--- ui-ux/src/App.jsx
+++ ui-ux/src/App.jsx
@@ -1,8 +1,13 @@
+import './App.css';
+import UrlShortener from "./components/UrlShortener";
+
 const App = () => {
+ 
   return (
-    <>
-    </>
-  )
-}
+    <div className="app">
+      <UrlShortener/>
+    </div>
+  );
+};
 
 export default App;
